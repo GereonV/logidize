12f35d044ace2a3e66e6dad1ffd696a65a36f533
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-04-02 17:30:29 +0200

    Test multi-threaded API

--- src/lib.rs
+++ src/lib.rs
@@ -35,10 +35,11 @@ pub trait Logger {
 mod tests {
     use std::time::SystemTime;
 
-    use crate::{Level, Logger, single_threaded::{LogObject, SimpleLogger}};
+    use crate::{Level, Logger};
 
     #[test]
     fn test_basic() {
+        use super::single_threaded::{LogObject, SimpleLogger};
         let start_time = SystemTime::now();
         let logger = SimpleLogger::new(|log_object: LogObject| {
             assert!(log_object.time >= start_time);
@@ -53,6 +54,7 @@ mod tests {
 
     #[test]
     fn test_channels() {
+        use super::single_threaded::{LogObject, SimpleLogger};
         let mut counter = 0;
         let logger = SimpleLogger::new(|log_object: LogObject| {
             assert_eq!(log_object.channel_id, Some(counter));
@@ -62,5 +64,54 @@ mod tests {
             let channel = logger.channel(i);
             channel.debug("message");
         }
+    }
+
+    #[test]
+    fn test_thread_simple() {
+        use super::multi_threaded::{LogObject, SimpleLogger};
+        let mut counter = 0;
+        let logger = SimpleLogger::new(|_log_object: LogObject| {
+            counter += 1;
+        });
+        std::thread::scope(|scope| {
+            for _ in 0..10 {
+                scope.spawn(|| {
+                    for _ in 0..100_000 {
+                        logger.debug("message");
+                    }
+                });
+            }
+        });
+        assert_eq!(counter, 10 * 100_000);
+    }
+
+    #[test]
+    fn test_thread_channel() {
+        use super::multi_threaded::{LogObject, SimpleLogger};
+        let mut counters = [0; 10];
+        let logger = SimpleLogger::new(|log_object: LogObject| {
+            let idx = match log_object.channel_id {
+                None => 0,
+                Some(id) => id,
+            };
+            counters[idx] += 1;
+        });
+        std::thread::scope(|scope| {
+            for i in 1..10 {
+                let channel = logger.channel(i);
+                scope.spawn(move || {
+                    for _ in 0..(i * 100_000) {
+                        channel.debug("message");
+                    }
+                });
+            }
+            for _ in 0..1_000_000 {
+                logger.debug("message");
+            }
+        });
+        assert_eq!(
+            counters,
+            [1_000_000, 100_000, 200_000, 300_000, 400_000, 500_000, 600_000, 700_000, 800_000, 900_000],
+        );
     }
 }
