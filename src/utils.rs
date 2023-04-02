fa4cbc63a7334cec5fac51edb190a66cc02a5f57
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-04-03 00:58:55 +0200

    Add test

--- src/utils.rs
+++ src/utils.rs
@@ -116,3 +116,34 @@ impl<W: Write, M: ChannelFilterMap> Sink for WriteSink<W, M> {
 		};
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use crate::single_threaded::SimpleLogger;
+	use super::*;
+
+	#[test]
+	fn test_basics() {
+		let mut output = Vec::<u8>::new();
+		let logger = SimpleLogger::new(WriteSink::new(&mut output, InvisibleChannelFilterMap));
+		debug!(logger, "debug");
+		info!(logger, "info");
+		warning!(logger, "warning");
+		error!(logger, "error");
+		critical!(logger, "critical");
+		debug!(logger.channel(1), "from channel {}", 1);
+		debug!(logger.channel(2), "from channel {}", 2);
+		let mut iter = output.iter();
+		let _ = iter.by_ref().find(|char| **char == b'm');
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..5]).unwrap(), "DEBUG");
+		let _ = iter.by_ref().find(|char| **char == b'm');
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..12]).unwrap(), "][0]: debug\n");
+		let _ = iter.by_ref().find(|char| **char == b'm');
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..4]).unwrap(), "INFO");
+		let _ = iter.by_ref().find(|char| **char == b':');
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..6]).unwrap(), " info\n");
+		let _ = iter.by_ref().filter(|char| **char == b'\n').take(4).last();
+		let _ = iter.by_ref().filter(|char| **char == b']').take(2).last();
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..20]).unwrap(), "[1]: from channel 1\n");
+	}
+}
