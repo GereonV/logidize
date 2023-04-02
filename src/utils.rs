b02afe45bc6ef0f6662c897d8cca0103706ef044
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-04-03 01:35:49 +0200

    Update tests

--- src/utils.rs
+++ src/utils.rs
@@ -142,16 +142,24 @@ mod tests {
 		debug!(logger.channel(1), "from channel {}", 1);
 		debug!(logger.channel(2), "from channel {}", 2);
 		let mut iter = output.iter();
-		let _ = iter.by_ref().find(|char| **char == b'm');
+		let _ = iter.by_ref().filter(|char| **char == b'm').take(3).last();
 		assert_eq!(std::str::from_utf8(&iter.as_slice()[..5]).unwrap(), "DEBUG");
-		let _ = iter.by_ref().find(|char| **char == b'm');
-		assert_eq!(std::str::from_utf8(&iter.as_slice()[..12]).unwrap(), "][0]: debug\n");
-		let _ = iter.by_ref().find(|char| **char == b'm');
+		let _ = iter.by_ref().filter(|char| **char == b'm').take(2).last();
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..1]).unwrap(), "0");
+		let _ = iter.by_ref().filter(|char| **char == b'm').take(1).last();
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..9]).unwrap(), "]: debug\n");
+
+		let _ = iter.by_ref().filter(|char| **char == b'm').take(3).last();
 		assert_eq!(std::str::from_utf8(&iter.as_slice()[..4]).unwrap(), "INFO");
-		let _ = iter.by_ref().find(|char| **char == b':');
-		assert_eq!(std::str::from_utf8(&iter.as_slice()[..6]).unwrap(), " info\n");
+		let _ = iter.by_ref().filter(|char| **char == b'm').take(2).last();
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..1]).unwrap(), "0");
+		let _ = iter.by_ref().filter(|char| **char == b'm').take(1).last();
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..8]).unwrap(), "]: info\n");
+
 		let _ = iter.by_ref().filter(|char| **char == b'\n').take(4).last();
-		let _ = iter.by_ref().filter(|char| **char == b']').take(2).last();
-		assert_eq!(std::str::from_utf8(&iter.as_slice()[..20]).unwrap(), "[1]: from channel 1\n");
+		let _ = iter.by_ref().filter(|char| **char == b'm').take(5).last();
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..1]).unwrap(), "1");
+		let _ = iter.by_ref().filter(|char| **char == b'm').take(1).last();
+		assert_eq!(std::str::from_utf8(&iter.as_slice()[..18]).unwrap(), "]: from channel 1\n");
 	}
 }
